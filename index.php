34d0f5fe4caf642c3b3cfdec198b890f9a0101f6
Matthew <wojack@wagecuck.club>
2020-08-16 17:08:40 +0100

    index: add title and language options
    You can now pick from 4 (four) language options for the index page:
    English, Irish (Gaelic), French (France) and Latin. You do this by
    entering your preferred language's ISO 639-1 code as a URL argument
    
    If no language is specified, or the requested language is not supported,
    the page will display all of the supported languages at once

--- index.php
+++ index.php
@@ -3,10 +3,26 @@
 <head>
 <link rel="icon" href="img/banner.png">
 <link rel="stylesheet" href="res/style.css">
-<title>lol</title>
+<title>By Matthew</title>
 </head>
 <body>
 <img src="img/banner.png" alt="banner"> 
-<header>Fáilte!</header>
+<?php
+$lang = $_GET['lang'];
+if (!$lang) {
+	echo "<header>Welcome! | Fáilte! | Gratissimum! | Bienvenue!</header>";
+} else if ($lang === "en") {
+	echo "<header>Welcome!</header>";
+} else if ($lang === "ga") {
+	echo "<header>Fáilte!</header>";
+} else if ($lang === "la") {
+	echo "<header>Gratissium!</header>";
+} else if ($lang === "fr") {
+	echo "<header>Bienvenue!</header>";
+} else {
+	echo "<header>Welcome! | Fáilte! | Gratissimum! | Bienvenue!</header>";
+}
+
+?>
 </body>
 </html>
