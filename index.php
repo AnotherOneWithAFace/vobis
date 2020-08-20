45eeb08da3502201b241109bf3a52ffa1214284a
Matthew <wojack@wagecuck.club>
2020-08-20 16:46:27 +0100

    index: refactor language choosing to use a switch statement
    Fixes #4

--- index.php
+++ index.php
@@ -9,20 +9,23 @@
 <img src="img/banner.png" alt="banner"> 
 <?php
 $lang = $_GET['lang'];
-if (!$lang) {
-	echo "<header>Welcome! | Fáilte! | Gratissimum! | Bienvenue!</header>";
-} else if ($lang === "en") {
-	echo "<header>Welcome!</header>";
-} else if ($lang === "ga") {
-	echo "<header>Fáilte!</header>";
-} else if ($lang === "la") {
-	echo "<header>Gratissium!</header>";
-} else if ($lang === "fr") {
-	echo "<header>Bienvenue!</header>";
-} else {
-	echo "<header>Welcome! | Fáilte! | Gratissimum! | Bienvenue!</header>";
+switch ($lang) {
+	case "en":
+		echo "<header>Welcome!</header>";
+		break;
+	case "ga":
+		echo "<header>Fáilte!</header>";
+		break;
+	case "la":
+		echo "<header>Gratissium!</header>";
+		break;
+	case "fr":
+		echo "<header>Bienvenue!</header>";
+		break;
+	default:
+		echo "<header>Welcome! | Fáilte! | Gratissimum! | Bienvenue!</header>";
+		break;
 }
-
 ?>
 </body>
 </html>
